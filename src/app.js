0a77e20ec0d2d592f840b8f88ffbceb7fb4fbff7
dvera2019 <134056369+dvera2019@users.noreply.github.com>
2023-06-24 23:27:28 +0100

    added some js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+function displayTemp(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.city;
+}
 
+let apiKey = "0bfe478a8b8a7te3aao74dc34b69a3b6";
+let query = "Paris";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemp);
