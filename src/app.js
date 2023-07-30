b89126dab40715b8180f9ac83a413fe38178ca26
dvera2019 <134056369+dvera2019@users.noreply.github.com>
2023-07-30 21:46:09 +0100

    added the day forecast

--- src/app.js
+++ src/app.js
@@ -15,6 +15,14 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
@@ -26,7 +34,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `         <div class="col-2">
-                <div class="weather-forecast-date">${formatDate(
+                <div class="weather-forecast-date">${formatDay(
                   forecastDay.time
                 )}</div>
                 <img
